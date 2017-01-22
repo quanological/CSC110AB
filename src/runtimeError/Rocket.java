79f4dd6921d6f18feae5a029d16d49537ffbcf78
Quan Truong <qmtruong92@gmail.com>
2017-01-21 23:38:01 -0700

    Initial commit

---
+++ src/runtimeError/Rocket.java
@@ -0,0 +1,27 @@
+package runtimeError;
+
+public class Rocket {
+
+	public static void main(String[] args) {
+		drawDiamond();
+		
+		/*drawX();
+		
+		drawRocket;*/
+		
+		int twoPi = 2; 
+		
+		System.out.println( ( /* 5 + 3 */ - 9 + 6/*8*//2 ));
+
+	}
+
+	
+	public static void drawDiamond() {
+		System.out.println("      /\\");
+		System.out.println("     /  \\");
+		System.out.println("    /    \\");
+		System.out.println("    \\    //");
+		System.out.println("     \\  //");
+		System.out.println("      \\//");
+	}
+}
---
+++ src/runtimeError/runTimeError.java
@@ -0,0 +1,15 @@
+package runtimeError;
+
+public class runTimeError {
+
+	public static void main(String[] args) {
+	
+			oops();
+	}
+	
+	public static void oops() {
+		System.out.println("Make it stop");
+		oops();
+	}
+
+}
