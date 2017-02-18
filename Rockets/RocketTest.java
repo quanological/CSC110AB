4bffbc4f0a6356c6eddf7a1faab53df4d0c4ee84
Quan Truong <qmtruong92@gmail.com>
2017-02-18 15:19:58 -0700

    Refactorized all methods (except main) to return String types

--- Rockets/RocketTest.java
+++ Rockets/RocketTest.java
@@ -8,13 +8,14 @@ package Rockets;
  * @author qmtru
  */
 
-
 public class RocketTest {
-	final static int SIZE = 40;
+	final static int SIZE = 4;
+
 	public static void main(String[] args) {
-		
-		//Alternating print and println because drawCone completes with a new line
-		//and next part does not
+
+		// Alternating print and println because drawCone completes with a new
+		// line
+		// and next part does not
 		System.out.print(drawCone());
 		System.out.println(drawLine());
 		System.out.print(drawUpperArrows());
@@ -25,60 +26,58 @@ public class RocketTest {
 		System.out.println(drawLine());
 		System.out.println(drawCone());
 	}
-	
+
 	public static String drawCone() {
-		
-		
+
 		int width = SIZE * 2 - 1;
 		String result = "";
-		
+
 		// columns
 		for (int line = width; line >= 1; line--) {
-	
+
 			// spaces
 			for (int space = 1; space <= line; space++) {
 				result += " ";
 			}
-	
+
 			// fslash
 			for (int fslash = line; fslash <= width; fslash++) {
 				result += "/";
 			}
-	
+
 			// stars
 			for (int stars = 1; stars <= 2; stars++) {
 				result += "*";
 			}
-	
+
 			// bslash
 			for (int bslash = line; bslash <= width; bslash++) {
-				result +=  "\\";
+				result += "\\";
 			}
-	
+
 			// spaces
 			for (int space = 1; space <= line; space++) {
 				result += " ";
 			}
-			
-			//print new line
+
+			// print new line
 			result += "\n";
-			
+
 		}
-		
+
 		return result;
 
-		
 	}
-	
+
 	public static String drawLine() {
-		
-		int width = (SIZE*4);
+
+		int width = (SIZE * 4);
 		String result = "";
 
 		result += "+";
-		
-		//odd is =
-		//even is *
+
+		// odd is =
+		// even is *
 		for (int line = 1; line <= width; line++) {
 			if (line % 2 == 1) {
 				result += "=";
@@ -86,108 +85,109 @@ public class RocketTest {
 				result += "*";
 			}
 		}
-		
+
 		result += "+";
 		return result;
 
 	}
-	
+
 	public static String drawUpperArrows() {
 		String result = "";
-	
-		//amount of columns is proportional to size
+
+		// amount of columns is proportional to size
 		for (int column = SIZE; column >= 1; column--) {
-			
-			//append the |
-			System.out.print("|");
-			
-			//amount of dots begins at 2 and ends when it hits the current column
-			//column decrements in value so the number of dots will decrease
-			//begins at 2 in order to align with SIZE number of columns
+
+			// append the |
+			result += "|";
+
+			// amount of dots begins at 2 and ends when it hits the current
+			// column
+			// column decrements in value so the number of dots will decrease
+			// begins at 2 in order to align with SIZE number of columns
 			for (int dots = 2; dots <= column; dots++) {
-				System.out.print(".");
+				result += ".";
 			}
-			
-			//amount of slashes begins at column (4), and tested against constant 4
-			//column decrements so slash will decrease in value
-			//then slash will count upwards to SIZE 
+
+			// amount of slashes begins at column (4), and tested against
+			// constant 4
+			// column decrements so slash will decrease in value
+			// then slash will count upwards to SIZE
 			for (int slashes = column; slashes <= SIZE; slashes++) {
-				System.out.print("/\\");
+				result += "/\\";
 			}
-			
-			//amount of dots can be achieved by taking the initial dots for loop
-			//and multiplying test value by 2 and subtracting 1
-			for (int dots = 2; dots <= ( ( column * 2 ) - 1 ); dots++) {
-				System.out.print(".");
+
+			// amount of dots can be achieved by taking the initial dots for
+			// loop
+			// and multiplying test value by 2 and subtracting 1
+			for (int dots = 2; dots <= ((column * 2) - 1); dots++) {
+				result += ".";
 			}
-			
-			//same slash loop as the initial slash loop
+
+			// same slash loop as the initial slash loop
 			for (int slashes = column; slashes <= SIZE; slashes++) {
-				System.out.print("/\\");
+				result += "/\\";
 			}
-			
-			//same dot loop as the initial dot loop
+
+			// same dot loop as the initial dot loop
 			for (int dots = 2; dots <= column; dots++) {
-				System.out.print(".");
+				result += ".";
 			}
-			
-			System.out.print("|");
-			
-			System.out.println();
+
+			result += "|";
+
+			result += "\n";
 		}
-		
-		
+
 		return result;
 	}
-	
+
 	public static String drawLowerArrows() {
-		
+
 		String result = "";
-		//amount of columns is proportional to size
+		// amount of columns is proportional to size
 		for (int column = 1; column <= SIZE; column++) {
-			
-			//append the |
-			System.out.print("|");
-			
-			//amount of dots begins at 2 and ends when it hits the current column
-			//column decrements in value so the number of dots will decrease
-			//begins at 2 in order to align with SIZE number of columns
+
+			// append the |
+			result += "|";
+
+			// amount of dots begins at 2 and ends when it hits the current
+			// column
+			// column decrements in value so the number of dots will decrease
+			// begins at 2 in order to align with SIZE number of columns
 			for (int dots = 2; dots <= column; dots++) {
-				System.out.print(".");
+				result += ".";
 			}
-			
-			//amount of slashes begins at column (4), and tested against constant 4
-			//column decrements so slash will decrease in value
-			//then slash will count upwards to SIZE 
+
+			// amount of slashes begins at column (4), and tested against
+			// constant 4
+			// column decrements so slash will decrease in value
+			// then slash will count upwards to SIZE
 			for (int slashes = column; slashes <= SIZE; slashes++) {
-				System.out.print("\\/");
+				result += "\\/";
 			}
-			
-			//amount of dots can be achieved by taking the initial dots for loop
-			//and multiplying test value by 2 and subtracting 1
-			for (int dots = 2; dots <= ( ( column * 2 ) - 1 ); dots++) {
-				System.out.print(".");
+
+			// amount of dots can be achieved by taking the initial dots for
+			// loop
+			// and multiplying test value by 2 and subtracting 1
+			for (int dots = 2; dots <= ((column * 2) - 1); dots++) {
+				result += ".";
 			}
-			
-			//same slash loop as the initial slash loop
+
+			// same slash loop as the initial slash loop
 			for (int slashes = column; slashes <= SIZE; slashes++) {
-				System.out.print("\\/");
+				result += "\\/";
 			}
-			
-			//same dot loop as the initial dot loop
+
+			// same dot loop as the initial dot loop
 			for (int dots = 2; dots <= column; dots++) {
-				System.out.print(".");
+				result += ".";
 			}
-			
-			System.out.print("|");
-			System.out.println();
+
+			result += "|";
+			result += "\n";
+
 		}
-		
-		
-		
-		
-		
 		return result;
 	}
-	
+
 }
