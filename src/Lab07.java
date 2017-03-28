a0813f1105f35cbb83fb1fabf7550c05107dfd43
quanological <qmtruong92@gmail.com>
2017-03-27 20:29:16 -0700

    Lab07

--- src/Lab07.java
+++ src/Lab07.java
@@ -2,90 +2,90 @@ package src;
 
 public class Lab07 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.out.println(sumSome(5, 18, 3));
+        System.out.println(sumSome(5, 18, 3));
 
-		System.out.println(isBalanced("aeioubdfgh"));
-		
-		System.out.println(merge("fort", "knox"));
+        System.out.println(isBalanced("aeioubdfgh"));
 
-	}
+        System.out.println(merge("fort", "knox"));
 
-	public static int sumSome(int range1, int range2, int divisor) {
+    }
 
-		int sum = 0;
-		for (int begin = range1; begin <= range2; begin++) {
-			if (begin % divisor == 0) {
-				sum += begin;
-			}
+    public static int sumSome(int range1, int range2, int divisor) {
 
-		}
-		return sum;
-	}
+        int sum = 0;
+        for (int begin = range1; begin <= range2; begin++) {
+            if (begin % divisor == 0) {
+                sum += begin;
+            }
 
-	public static boolean isBalanced(String upperCaseCheck) {
+        }
+        return sum;
+    }
 
-		String upperCaseupperCaseCheck = upperCaseCheck.toUpperCase();
+    public static boolean isBalanced(String upperCaseCheck) {
 
-		int vowelCount, consonantCount;
-		vowelCount = 0;
-		consonantCount = 0;
+        String upperCaseupperCaseCheck = upperCaseCheck.toUpperCase();
 
-		// loop through the string and count number of vowels
-		for (int i = 0; i < upperCaseupperCaseCheck.length(); i++) {
-			if ((upperCaseCheck.charAt(i) == 'a') || (upperCaseCheck.charAt(i) == 'e')
-					|| (upperCaseCheck.charAt(i) == 'i')
-					|| (upperCaseCheck.charAt(i) == 'o' || (upperCaseCheck.charAt(i) == 'u'))) {
-				vowelCount += 1;
-			}
-		}
+        int vowelCount, consonantCount;
+        vowelCount = 0;
+        consonantCount = 0;
 
-		// check for consonants
-		for (int i = 0; i < upperCaseCheck.length(); i++) {
-			if ((upperCaseCheck.toUpperCase().charAt(i) == 'B') || (upperCaseCheck.toUpperCase().charAt(i) == 'C')
-					|| (upperCaseCheck.toUpperCase().charAt(i) == 'D')
-					|| (upperCaseCheck.toUpperCase().charAt(i) == 'F')
-					|| (upperCaseCheck.toUpperCase().charAt(i) == 'G')
-					|| (upperCaseCheck.toUpperCase().charAt(i) == 'H')
-					|| (upperCaseCheck.toUpperCase().charAt(i) == 'J')
-					|| (upperCaseCheck.toUpperCase().charAt(i) == 'K')
-					|| (upperCaseCheck.toUpperCase().charAt(i) == 'L')
-					|| (upperCaseCheck.toUpperCase().charAt(i) == 'M')
-					|| (upperCaseCheck.toUpperCase().charAt(i) == 'N')
-					|| (upperCaseCheck.toUpperCase().charAt(i) == 'P')
-					|| (upperCaseCheck.toUpperCase().charAt(i) == 'Q')
-					|| (upperCaseCheck.toUpperCase().charAt(i) == 'R')
-					|| (upperCaseCheck.toUpperCase().charAt(i) == 'S')
-					|| (upperCaseCheck.toUpperCase().charAt(i) == 'T')
-					|| (upperCaseCheck.toUpperCase().charAt(i) == 'V')
-					|| (upperCaseCheck.toUpperCase().charAt(i) == 'X')
-					|| (upperCaseCheck.toUpperCase().charAt(i) == 'Z')) {
-				consonantCount += 1;
-			}
-		}
+        // loop through the string and count number of vowels
+        for (int i = 0; i < upperCaseupperCaseCheck.length(); i++) {
+            if ((upperCaseCheck.charAt(i) == 'a') || (upperCaseCheck.charAt(i) == 'e')
+                    || (upperCaseCheck.charAt(i) == 'i')
+                    || (upperCaseCheck.charAt(i) == 'o' || (upperCaseCheck.charAt(i) == 'u'))) {
+                vowelCount += 1;
+            }
+        }
 
-		return (vowelCount == consonantCount);
+        // check for consonants
+        for (int i = 0; i < upperCaseCheck.length(); i++) {
+            if ((upperCaseCheck.toUpperCase().charAt(i) == 'B') || (upperCaseCheck.toUpperCase().charAt(i) == 'C')
+                    || (upperCaseCheck.toUpperCase().charAt(i) == 'D')
+                    || (upperCaseCheck.toUpperCase().charAt(i) == 'F')
+                    || (upperCaseCheck.toUpperCase().charAt(i) == 'G')
+                    || (upperCaseCheck.toUpperCase().charAt(i) == 'H')
+                    || (upperCaseCheck.toUpperCase().charAt(i) == 'J')
+                    || (upperCaseCheck.toUpperCase().charAt(i) == 'K')
+                    || (upperCaseCheck.toUpperCase().charAt(i) == 'L')
+                    || (upperCaseCheck.toUpperCase().charAt(i) == 'M')
+                    || (upperCaseCheck.toUpperCase().charAt(i) == 'N')
+                    || (upperCaseCheck.toUpperCase().charAt(i) == 'P')
+                    || (upperCaseCheck.toUpperCase().charAt(i) == 'Q')
+                    || (upperCaseCheck.toUpperCase().charAt(i) == 'R')
+                    || (upperCaseCheck.toUpperCase().charAt(i) == 'S')
+                    || (upperCaseCheck.toUpperCase().charAt(i) == 'T')
+                    || (upperCaseCheck.toUpperCase().charAt(i) == 'V')
+                    || (upperCaseCheck.toUpperCase().charAt(i) == 'X')
+                    || (upperCaseCheck.toUpperCase().charAt(i) == 'Z')) {
+                consonantCount += 1;
+            }
+        }
 
-	}
+        return (vowelCount == consonantCount);
 
-	public static String merge(String a, String b) {
+    }
 
-		String mergedString = "";
+    public static String merge(String a, String b) {
 
-		for (int i = 0; i <= a.length() - 1; i++) {
-			if (i % 2 == 0) {
-				mergedString += a.charAt(i);
-				mergedString += b.charAt(i);
-			} else if ( i % 2 != 0) {
-				mergedString += a.charAt(i);
-				mergedString += b.charAt(i);
-			}
+        String mergedString = "";
 
-		}
+        for (int i = 0; i <= a.length() - 1; i++) {
+            if (i % 2 == 0) {
+                mergedString += a.charAt(i);
+                mergedString += b.charAt(i);
+            } else if (i % 2 != 0) {
+                mergedString += a.charAt(i);
+                mergedString += b.charAt(i);
+            }
 
-		return mergedString;
+        }
 
-	}
+        return mergedString;
+
+    }
 
 }
