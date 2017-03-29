16b69c3586055175c7830163c820d053ffae495f
quanological <qmtruong92@gmail.com>
2017-03-29 00:00:52 -0700

    Lab10

---
+++ src/Lab10.java
@@ -0,0 +1,76 @@
+/**
+ * Programmer: Quan Truong
+ * Assignment: Lab10
+ * Date: March 28, 2017
+ * Professor: Dr. Zerangue
+ * Class: CSC110AB 12:00 - 12:50 PM
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Lab10 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        PrintStream ps = new PrintStream("/Users/qmtruong92/Desktop/newWords.txt");
+        File file = new File("/Users/qmtruong92/CSC110AB/src/words.txt");
+        Scanner in = new Scanner(file);
+
+
+        // Replace the text in the file with the new text from leetSpeak
+        leetSpeak(in, ps);
+
+    }
+
+    /**
+     * Reads a file and changes certain characters in it according to the conditions below.
+     *
+     * @param in  Scanner. Location of text retrieval.
+     * @param out PrintStream. Destination of where output is to be written.
+     */
+    public static void leetSpeak(Scanner in, PrintStream out) {
+
+        // Executes if the Scanner has another line
+        while (in.hasNextLine()) {
+
+
+            String currentLine = in.nextLine();
+            Scanner fromFile = new Scanner(currentLine);
+
+            // Replace text in each word as long as there are tokens
+            while (fromFile.hasNext()) {
+
+                String word = fromFile.next();
+
+                if (word.contains("o")) {
+                    word = word.replace("o", "0");
+                }
+                if (word.contains("l")) {
+                    word = word.replace('l', '1');
+                }
+                if (word.contains("e")) {
+                    word = word.replace('e', '3');
+                }
+                if (word.contains("a")) {
+                    word = word.replace('a', '4');
+                }
+                if (word.contains("t")) {
+                    word = word.replace('t', '7');
+                }
+                if (word.contains("s")) {
+                    word = word.replace('s', 'z');
+                }
+                // last letter
+                if (word.charAt(word.length() - 1) == 's') {
+                    word = word.substring(0, word.length() - 1) + "Z";
+                }
+
+                out.print("(" + word + ")" + " ");
+
+            }
+            out.println();
+        }
+
+
+    }
+}
