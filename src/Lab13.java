be38ea980c1e4a87fb9a4d3fa6395788e7e89ddb
quanological <qmtruong92@gmail.com>
2017-04-13 11:09:05 -0700

    Lab13

---
+++ src/Lab13.java
@@ -0,0 +1,60 @@
+/**
+ * Programmmer: Quan Truong
+ * Professor: Dr. Zerangue
+ * Class: CSC110AB
+ * Date: April 13, 2017
+ * <p>
+ * This program returns the last index of a parameter in an array, and also gets the median number that occurs
+ * in an array.
+ */
+
+import java.util.Arrays;
+
+public class Lab13 {
+
+    public static void main(String[] args) {
+
+        int[] arrayOfNums = {7, 9, 2, 4, 3};
+
+        System.out.println(lastIndexOf(arrayOfNums, 2));
+
+        System.out.println(median(arrayOfNums));
+    }
+
+    /**
+     * Returns the last index in which a parameter appears in an array.
+     * @param array The array to be searched.
+     * @param parameter The parameter to be found in an array.
+     * @return Integer. The last index in which a parameter appears.
+     */
+    public static int lastIndexOf(int[] array, int parameter) {
+        int theIndex = 0;
+        for (int count = 0; count < array.length; count++) {
+            if (array[count] > parameter) {
+                theIndex = count;
+            }
+        }
+        return theIndex;
+    }
+
+    /**
+     * Returns the middle number in an odd array.
+     * @param array Integer Array. The integer array to be searched for a median number.
+     * @return The median number in an array.
+     */
+    public static int median(int[] array) {
+        int divideBy2 = array.length / 2;
+        for (int i = array.length; i > 1; i--) {
+            for (int count = 0; count < array.length - 1; count++) {
+                int temp, temp1;
+                if (array[count] > array[count + 1]) {
+                    temp = array[count + 1];
+                    temp1 = array[count];
+                    array[count] = temp;
+                    array[count + 1] = temp1;
+                }
+            }
+        }
+        return array[divideBy2];
+    }
+}
